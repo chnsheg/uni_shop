cf99de794eadff3d0000516f7adc4e066306c1e1
chnsheg <1445001103@qq.com>
2022-12-08 12:33:48 +0800

    完成home首页的开发

--- main.js
+++ main.js
@@ -2,6 +2,29 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+import {$http} from '@escook/request-miniprogram'
+
+uni.$http = $http
+$http.baseUrl = 'http://api-hmugo-web.itheima.net'
+
+$http.beforeRequest = function(option) {
+  uni.showLoading({
+    title:'数据加载中。。。'
+  })
+}
+
+$http.afterRequest = function(){
+  uni.hideLoading()
+}
+
+// 封装的展示消息提示的方法
+uni.$showMsg = function (title = '数据加载失败！', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none',
+  })
+}
 
 Vue.config.productionTip = false
 
