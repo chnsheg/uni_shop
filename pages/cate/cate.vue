6e839d72ef189a907133bb9dcaf950fa7c3e33fd
chnsheg <1445001103@qq.com>
2022-12-08 19:10:30 +0800

    完成了搜索的功能

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,5 +1,7 @@
 <template>
   <view>
+    <!-- 使用自定义的搜索组件 -->
+    <my-search @click="gotoSearch"></my-search>
     <view class="scroll-view-container">
       <!-- 左侧的滚动视图区域 -->
       <scroll-view class="left-scroll-view" scroll-y :style="{height: wh + 'px'}">
@@ -19,7 +21,7 @@
             <view class="cate-lv3-item" v-for="(item3, i3) in item2.children" :key="i3" @click="gotoGoodsList(item3)">
               <image :src="item3.cat_icon"></image>
               <text>{{item3.cat_name}}</text>
-            </view> 
+            </view>
           </view>
         </view>
       </scroll-view>
@@ -46,35 +48,45 @@
       // 获取当前系统的信息
       const sysInfo = uni.getSystemInfoSync()
       // 为 wh 窗口可用高度动态赋值
-      this.wh = sysInfo.windowHeight
+      // 可用高度 = 屏幕高度 - navigationBar高度 - tabBar高度 - 自定义的search组件高度
+      this.wh = sysInfo.windowHeight - 50
       // 调用获取分类列表数据的方法
       this.getCateList()
     },
 
     methods: {
       // 选中项改变的事件处理函数
-        activeChanged(i) {
-          this.active = i
-          // 为二级分类列表重新赋值
-          this.cateLevel2 = this.cateList[i].children
-          // 让 scrollTop 的值在 0 与 1 之间切换
-          this.scrollTop = this.scrollTop === 0 ? 1 : 0
-        },
-        
-        async getCateList() {
-          const { data: res } = await uni.$http.get('/api/public/v1/categories')
-          if (res.meta.status !== 200) return uni.$showMsg()
-          this.cateList = res.message
-          // 为二级分类赋值
-          this.cateLevel2 = res.message[0].children
-        },
-        
-        // 点击三级分类项跳转到商品列表页面
-        gotoGoodsList(item3) {
-          uni.navigateTo({
-            url: '/subpkg/goods_list/goods_list?cid=' + item3.cat_id
-          })
-        }
+      activeChanged(i) {
+        this.active = i
+        // 为二级分类列表重新赋值
+        this.cateLevel2 = this.cateList[i].children
+        // 让 scrollTop 的值在 0 与 1 之间切换
+        this.scrollTop = this.scrollTop === 0 ? 1 : 0
+      },
+
+      // 跳转到分包中的搜索页面
+      gotoSearch() {
+        uni.navigateTo({
+          url: '/subpkg/search/search'
+        })
+      },
+
+      async getCateList() {
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/categories')
+        if (res.meta.status !== 200) return uni.$showMsg()
+        this.cateList = res.message
+        // 为二级分类赋值
+        this.cateLevel2 = res.message[0].children
+      },
+
+      // 点击三级分类项跳转到商品列表页面
+      gotoGoodsList(item3) {
+        uni.navigateTo({
+          url: '/subpkg/goods_list/goods_list?cid=' + item3.cat_id
+        })
+      }
     }
 
   }
@@ -114,30 +126,30 @@
       }
     }
   }
-  
+
   .cate-lv2-title {
     font-size: 12px;
     font-weight: bold;
     text-align: center;
     padding: 15px 0;
   }
-  
+
   .cate-lv3-list {
     display: flex;
     flex-wrap: wrap;
-  
+
     .cate-lv3-item {
       width: 33.33%;
       margin-bottom: 10px;
       display: flex;
       flex-direction: column;
       align-items: center;
-  
+
       image {
         width: 60px;
         height: 60px;
       }
-  
+
       text {
         font-size: 12px;
       }
